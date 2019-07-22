32c4314249f2142df9bea17b5676f3feb1470a7d
DESKTOP-C16P34B\jkf <470778561@qq.com>
2019-07-22 22:45:31 +0800

    add  createStore

---
+++ redux/createStore.js
@@ -0,0 +1,37 @@
+export default function createStore(reducer) {
+    let state;
+    let listeners = [];//事件订阅
+
+    //返回当前的状态
+    function getState(){
+        return state;
+    }
+
+    //订阅事件，用于状态变更，触发view渲染。返回一个取消订阅的方法
+    function subscribe(listener){
+        listeners.push(listener);
+
+        //取消事件订阅
+        return function(){
+            listeners = listeners.filter(function (item){
+                return item !== listener;
+            })
+        }
+    }
+
+    function dispatch(action){
+        state = reducer(state, action);//获取新的状态
+        //发布事件  触发view更新
+        listeners.forEach(function(listener){
+            listener();
+        })
+    }
+
+    dispatch({ type: "@@TYEP/REDUX_INIT" });//初始化数据 state赋值为reducer中的默认值
+
+    return {
+        dispatch,
+        subscribe,
+        getState,
+    }
+}
